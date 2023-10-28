6eb6dd1fdd823e0b828cb47502ac54a3e86b3c7c
Muhammad Rehan <rehanch642@gmail.com>
2023-10-28 22:02:31 +0500

    find user before show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 # users controller for public users to show user profile and listing
 
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show]
 
     def index
         @users = User.all
     end
 
-    def show
+    def show; end
+
+    private
+
+    def find_user
         @user = User.friendly.find(params[:id])
     end
 end
