1a639b72651e423ffe2eb8c5fc63c8a8a10cbef7
Muhammad Rehan <rehanch642@gmail.com>
2023-10-30 22:32:47 +0500

    partial for new comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_25_051939) do
+ActiveRecord::Schema.define(version: 2023_10_30_172948) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_10_25_051939) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["parent_id"], name: "index_comments_on_parent_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
