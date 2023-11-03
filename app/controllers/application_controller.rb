e85f89a12f05c13ddbfdf41f0415664e5c695876
Muhammad Rehan <rehanch642@gmail.com>
2023-11-03 07:11:07 +0500

    generate and include pundit policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 # application controller
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
