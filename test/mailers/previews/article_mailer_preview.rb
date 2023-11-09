6ed70fe360032999df28d69e4d3a6b5abf3accf7
Muhammad Rehan <rehanch642@gmail.com>
2023-11-09 08:34:49 +0500

    preview mail

--- test/mailers/previews/article_mailer_preview.rb
+++ test/mailers/previews/article_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/article_mailer
 class ArticleMailerPreview < ActionMailer::Preview
-
+    def new_article
+        ArticleMailer.with(user: User.last).new_article
+      end
 end
