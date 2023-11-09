f3d4f3b8aafb688fdf1ce7d91ae4c47d32ed139b
Muhammad Rehan <rehanch642@gmail.com>
2023-11-09 08:34:34 +0500

    mail for new article

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,2 +1,8 @@
 class ArticleMailer < ApplicationMailer
+    def new_article
+        @user = params[:user]
+        @receivers = @user.followers
+        @article = params[:article]
+        mail(to: @receivers.pluck(:email), subject: 'Latest Article Published')
+      end
 end
