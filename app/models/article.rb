b6ce23bbb170e5ee6afcd26132582eea3697a801
Muhammad Rehan <rehanch642@gmail.com>
2023-11-20 04:52:22 +0500

    syntax fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :history
 
-  belongs_to :user, inverse_of: :article
+  belongs_to :user, inverse_of: :articles
   has_many :comments, dependent: :destroy, inverse_of: :article
 
   validates :title, :content, :user_id, presence: true
