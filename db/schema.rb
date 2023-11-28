2b9b9de3c9437cb1c401a8a35b4edb27913fefa3
Muhammad Rehan <rehanch642@gmail.com>
2023-11-28 20:27:14 +0500

    add schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_15_020229) do
+ActiveRecord::Schema.define(version: 2023_11_28_152618) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2023_11_15_020229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
