7806924872ed2eb156cc0459fc4c770cac5691e3
Muhammad Rehan <rehanch642@gmail.com>
2023-11-28 20:21:55 +0500

    find or create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,17 @@ class User < ApplicationRecord
   def slug_candidate
     ["#titan-#{full_name}"]
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+end
 end
