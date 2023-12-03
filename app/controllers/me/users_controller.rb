aaa8f33a84be337889cbb1c630221f56b17f16c3
Muhammad Rehan <rehanch642@gmail.com>
2023-12-03 19:53:58 +0500

    edit action

--- app/controllers/me/users_controller.rb
+++ app/controllers/me/users_controller.rb
@@ -2,5 +2,13 @@
 
 module Me
   class UsersController < ApplicationController
+    before_action :set_user, only: %I[:edit]
+    def edit; end
+
+    private
+
+    def set_user
+      @user = current_user
+    end
   end
 end
