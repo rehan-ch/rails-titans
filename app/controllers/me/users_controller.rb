c556b1a801c9272feb78556317e46a023e9a2e17
Muhammad Rehan <rehanch642@gmail.com>
2023-12-05 23:26:51 +0500

    user update form

--- app/controllers/me/users_controller.rb
+++ app/controllers/me/users_controller.rb
@@ -2,11 +2,19 @@
 
 module Me
   class UsersController < ApplicationController
-    before_action :set_user, only: %I[:edit]
+    before_action :set_user, only: %I[edit update]
     def edit; end
+
+    def update
+      current_user.update(user_params)
+      redirect_to :back
+    end
 
     private
 
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
     def set_user
       @user = current_user
     end
